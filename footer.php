8759eb20dc771d589b2e7cd89d2f0336e65b312d
mygoare <mygoare@gmail.com>
2012-01-09 20:36:27 +0800

    add the sinaapp icon logo

--- footer.php
+++ footer.php
@@ -3,7 +3,8 @@
 if(!defined('EMLOG_ROOT')) {exit('error!');} 
 ?>
 		<div id="footer">
-			<p id="copy">&copy; 2012 All rights resverd. Powered by mygoare</p>
+			<p id="copy">&copy; 2012 All rights resverd. <a href="http://sae.sina.com.cn" target="_blank"><img src="http://static.sae.sina.com.cn/image/poweredby/117X12px.gif" title="Powered by Sina App Engine"></a>
+ Powered by mygoare. </p>
 			<p>
 				<a href="">About</a>
 				<a href="">Works</a>
