8a7f58c4f095b06f3ac2601621a93f50da970e35
mygoare <mygoare@gmail.com>
2012-01-11 03:43:39 +0800

    make some change

--- echo_log.php
+++ echo_log.php
@@ -11,9 +11,6 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 	<?php echo $log_content; ?>
 				</div>
 			</div>
-			<div id="sidebar">
-				<?php doAction('log_related', $logData); ?>
-			</div>
 		</div>
 		<div class="clear"></div>
 <?php
